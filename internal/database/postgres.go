83fbf78b07ea8dd4f6ae62e1726915a1ae64d227
Lispa <dmitrynsu@gmail.com>
2026-03-05 03:39:18 +0200

    fix: construct connection string from env vars

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -8,10 +8,15 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-// Connect establishes a connection to the database
 func Connect() (*pgx.Conn, error) {
-	// Extract the connection string from .env
-	connStr := os.Getenv("DB_URL")
+	// Собираем строку формата postgres://username:password@host:port/dbname
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 
 	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
